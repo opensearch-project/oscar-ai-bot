22d5242997369908474f1c4052065da3e187f6f5
Veema Jhagru <veemaj@amazon.com>
2026-05-18 11:48:16 -0700

    Fix isort formatting
    Signed-off-by: Veema Jhagru <veemaj@amazon.com>

--- tests/agents/security_advisories/test_neglected_url.py
+++ tests/agents/security_advisories/test_neglected_url.py
@@ -26,8 +26,8 @@ _LAMBDA_PATH = os.path.join(
 if _LAMBDA_PATH not in sys.path:
     sys.path.insert(0, _LAMBDA_PATH)
 
-from response_filter import (NEGLECTED_PAGE_BASE,  # noqa: E402
-                             VALID_AGE_VALUES, build_neglected_page_url)
+from response_filter import NEGLECTED_PAGE_BASE  # noqa: E402
+from response_filter import VALID_AGE_VALUES, build_neglected_page_url
 
 # ---------------------------------------------------------------------------
 # Property 2: Neglected page URL round-trip
